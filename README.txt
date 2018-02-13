1fa195b63c74ae3369912d052385919c844b0c37
mrTestIsTheBest <michal.rusek@yahoo.com>
2018-02-13 16:31:54 +0100

    First commit

---
+++ README.txt
@@ -0,0 +1,1 @@
+Small admin dashboard template.
---
+++ scripts.js
@@ -0,0 +1,59 @@
+//Hides the menubar
+function hideMenuBar(){
+	//Get the menubar div
+	let menu = document.getElementById('menu')
+
+	//Hide it
+	menu.className = 'invisibleMenu'
+}
+
+//Shows the menubar
+function showMenuBar(){
+	//Get the menubar div
+	let menu = document.getElementById('menu')
+
+	//Show it
+	menu.className = 'visibleMenu'
+}
+
+//Shows/hides (toggles) the menubar
+function toggleMenuBar(){
+	//Get the menubar div
+	let menu = document.getElementById('menu')
+
+	//Check if it's visible
+	let isVisible = menu.className === 'visibleMenu'
+
+	//Toggle it
+	if(isVisible){
+		hideMenuBar()
+	}
+	else{
+		showMenuBar()
+	}
+}
+
+//Handles various resizing operations
+function handleWindowResize(){
+	//Hide the menubar if we hit <768px
+	if(document.body.clientWidth < 768){
+		let menu = document.getElementById('menu')
+		hideMenuBar(menu)
+	}
+}
+
+window.onload = ()=>{
+	//Add an event listener on show/hide menu button
+	document.getElementById('menuButton').addEventListener('click', toggleMenuBar)
+
+	//Add an event listener to the contentWrapper as well
+	//for mobile - if we click on the contentWrapper we want the menu to 
+	//disappear, etc.
+	document.getElementById('contentWrapperInner').addEventListener('click', hideMenuBar)
+
+	//Add an event listener on resize event
+	window.addEventListener('resize', handleWindowResize)
+
+	//Handle the "resize" at the very beggining
+	handleWindowResize()
+}
